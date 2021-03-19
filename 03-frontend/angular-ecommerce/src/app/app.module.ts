c41dfd651648ad5467f0896f77555eed01542069
kaustav1616 <kaustav1616@yahoo.com>
2021-03-19 19:55:01 +0530

    node dependencies

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -73,7 +73,7 @@ const routes: Routes = [
 >>>>>>> f96ce675f9be1d6a30e70a19aebe187be7d66e4d
   ],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     BrowserModule,
     HttpClientModule,
 <<<<<<< HEAD
